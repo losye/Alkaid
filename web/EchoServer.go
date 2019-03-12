0208bdaecce51db40553f29013f5adf6af22ed60
modong@pinduoduo.com <zhengye.zhang@owitho.com>
2019-03-12 22:25:02 +0800

    ad echo server

---
+++ web/EchoServer.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+	"fmt"
+)
+
+var cnt int
+var lock sync.Mutex
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
+
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	lock.Lock()
+	cnt ++
+	lock.Unlock()
+	fmt.Fprintf(writer, request.URL.Path)
+}
+
+func counter(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Counter %d\n", cnt)
+}
